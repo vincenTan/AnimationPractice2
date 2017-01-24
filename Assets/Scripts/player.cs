77cb311f631343f01687909f3aeddc22f8fdc629
Vincent Tan <vtgaming@outlook.com>
2017-01-23 22:33:50 -0500

    Animation practice project
    Unity-chan can move around freely, added features to AnimationPractice.

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    public Animator anim;
+    public Rigidbody rbody;
+
+    private float inputH;
+    private float inputV;
+    private bool run;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        rbody = GetComponent<Rigidbody>();
+        run = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if((Input.GetKey(KeyCode.LeftControl)) && (Input.GetKeyDown(KeyCode.Alpha1))) //ctrl+1 makes dance 1
+        {
+            anim.Play("WAIT01", -1, 0f);
+        }
+        if ((Input.GetKey(KeyCode.LeftControl)) && (Input.GetKeyDown(KeyCode.Alpha2))) //ctrl+2 makes dance 2
+        {
+            anim.Play("WAIT02", -1, 0f);
+        }
+        if ((Input.GetKey(KeyCode.LeftControl)) && (Input.GetKeyDown(KeyCode.Alpha3))) //ctrl+3 makes dance 3
+        {
+            anim.Play("WAIT03", -1, 0f);
+        }
+        if ((Input.GetKey(KeyCode.LeftControl)) && (Input.GetKeyDown(KeyCode.Alpha4))) //ctrl+4 makes dance 4
+        {
+            anim.Play("WAIT04", -1, 0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.L)) // damage animation
+        {
+            int n = Random.Range(0, 2);
+            if (n == 0)
+            {
+                anim.Play("DAMAGED00",-1,0f);
+            }
+            else
+            {
+                anim.Play("DAMAGED01", -1, 0f);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift)) //hold shift to run
+        {
+            run = true;
+        }else
+        {
+            run = false;
+        }
+
+        if (Input.GetKey(KeyCode.Space)) //press space to jump
+        {
+            anim.SetBool("jump", true);
+        }else
+        {
+            anim.SetBool("jump", false);
+        }
+
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+
+        anim.SetFloat("inputH", inputH);
+        anim.SetFloat("inputV", inputV);
+        anim.SetBool("run", run);
+
+        float moveX = inputH * 20f * Time.deltaTime;
+        float moveZ = inputV * 50f * Time.deltaTime;
+
+        if(moveZ <= 0)
+        {
+            moveX = 0f;
+        }else if(run)
+        {
+            moveX *= 3f;
+            moveZ *= 3f;
+        }
+
+        rbody.velocity = new Vector3(moveX, 0f, moveZ);
+    }
+}
---
+++ README.txt
@@ -0,0 +1,4 @@
+This is my third Unity project. It was created by referencing:
+https://www.youtube.com/watch?v=7NktwerZFro
+
+*****INCOMPLETE*****
